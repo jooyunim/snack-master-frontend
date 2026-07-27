55b8c1063f049f07b088c08ea036b9694a453fc6
yoonleejun <yoonjjang53@naver.com>
2026-07-27 18:14:50 +0900

    feat:pull 받기 전 커밋

--- src/app/(private)/purchase-request-manage/page.tsx
+++ src/app/(private)/purchase-request-manage/page.tsx
@@ -1,72 +1,22 @@
+"use client"
 import Button from '@/components/Button';
 import Pagination from '@/components/Pagination';
 import SortDropdown from '@/components/SortDropdown';
-import type { PurchaseRequest } from '@/features/purchase-request/types/purchase-request.types';
+import { useRequestList } from '@/features/purchase-request-manage/hooks/useRequestList';
+import { sortByOption } from '@/features/purchase-request-manage/types/purchase-request-manage.type';
+import { useState } from 'react';
 
-const REQUESTS: PurchaseRequest[] = [
-  {
-    id: 1,
-    date: '2024. 07. 04',
-    product: '코카콜라 제로 외 1건',
-    productName: '코카콜라 제로',
-    amount: '1,900',
-    status: 'pending',
-    requesterName: '김스낵',
-    requesterInitials: 'JN',
-  },
-  {
-    id: 2,
-    date: '2024. 07. 04',
-    product: '코카콜라 제로 외 1건',
-    productName: '코카콜라 제로',
-    amount: '1,900',
-    status: 'pending',
-    requesterName: '김스낵',
-    requesterInitials: 'JN',
-  },
-  {
-    id: 3,
-    date: '2024. 07. 04',
-    product: '코카콜라 제로 외 1건',
-    productName: '코카콜라 제로',
-    amount: '1,900',
-    status: 'pending',
-    requesterName: '김스낵',
-    requesterInitials: 'JN',
-  },
-  {
-    id: 4,
-    date: '2024. 07. 04',
-    product: '코카콜라 제로 외 1건',
-    productName: '코카콜라 제로',
-    amount: '1,900',
-    status: 'pending',
-    requesterName: '김스낵',
-    requesterInitials: 'JN',
-  },
-  {
-    id: 5,
-    date: '2024. 07. 04',
-    product: '코카콜라 제로 외 1건',
-    productName: '코카콜라 제로',
-    amount: '1,900',
-    status: 'pending',
-    requesterName: '김스낵',
-    requesterInitials: 'JN',
-  },
-  {
-    id: 6,
-    date: '2024. 07. 04',
-    product: '코카콜라 제로 외 1건',
-    productName: '코카콜라 제로',
-    amount: '1,900',
-    status: 'pending',
-    requesterName: '김스낵',
-    requesterInitials: 'JN',
-  },
-];
+
 
 export default function PurchaseRequestManagePage() {
+
+  const [sortBy, setsortBy] = useState<sortByOption>('recent')
+
+  const { data, isPending, isError } = useRequestList(sortBy)
+
+  if (isPending) return <div>로딩 중...</div>
+  if (isError) return <div>에러남</div>
+
   return (
     <div className="min-h-screen bg-white">
       <main className="mx-auto flex w-full max-w-[1400px] flex-col gap-10 px-6 pb-20 pt-20 max-lg:gap-5 max-lg:pt-[60px] max-sm:gap-3 max-sm:pt-10">
@@ -98,25 +48,23 @@ export default function PurchaseRequestManagePage() {
               </span>
             </div>
 
+
             <ul className="flex w-full min-w-[1100px] flex-col max-lg:min-w-[696px]">
-              {REQUESTS.map((request) => (
+              {data.map((request) => (
                 <li
                   key={request.id}
                   className="flex h-[100px] w-full items-center gap-20 border-b border-solid border-gray-100 px-10 max-lg:justify-between max-lg:gap-0 max-lg:px-0"
                 >
                   <span className="w-[142px] shrink-0 text-[16px] tracking-[-0.4px] text-gray-950 max-lg:w-[100px]">
-                    {request.date}
+                    {request.requestedAt}
                   </span>
                   <span className="w-[360px] shrink-0 text-[16px] tracking-[-0.4px] text-gray-950 max-lg:w-[140px]">
-                    {request.product}
+                    {request.itemSummary}
                   </span>
                   <span className="w-[142px] shrink-0 text-[16px] tracking-[-0.4px] text-gray-950 max-lg:w-[100px]">
-                    {request.amount}
+                    {request.totalAmount}
                   </span>
                   <div className="flex w-[134px] shrink-0 items-center gap-3 max-lg:w-[108px]">
-                    <span className="relative flex size-8 shrink-0 items-center justify-center overflow-hidden rounded-full bg-gray-50 text-[10px] tracking-[-0.25px] text-black">
-                      {request.requesterInitials}
-                    </span>
                     <span className="w-[90px] text-[16px] tracking-[-0.4px] text-gray-950 max-lg:w-16">
                       {request.requesterName}
                     </span>
