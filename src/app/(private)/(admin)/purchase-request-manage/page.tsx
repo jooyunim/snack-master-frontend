6cb2420e3c5a783607c3680b1a80c9f1f129f0c1
yoonleejun <yoonjjang53@naver.com>
2026-07-30 17:06:09 +0900

    feat:pull 받기 전 커밋

--- src/app/(private)/(admin)/purchase-request-manage/page.tsx
+++ src/app/(private)/(admin)/purchase-request-manage/page.tsx
@@ -1,19 +1,26 @@
 "use client"
 import Button from '@/components/Button';
 import Pagination from '@/components/Pagination';
-import SortDropdown from '@/components/SortDropdown';
+import SortDropdown, { SortOption } from '@/components/SortDropdown';
 import { useRequestList } from '@/features/purchase-request-manage/hooks/useRequestList';
 import { ModalState, sortByOption } from '@/features/purchase-request-manage/types/purchase-request-manage.type';
 import { useState } from 'react';
 import PurchaseRequestModal from './components/PurchaseRequestModal';
 
 
-
 export default function PurchaseRequestManagePage() {
+
+  const SORT_OPTIONS: SortOption[] = [
+    { label: '최신순', value: 'recent' },
+    { label: '낮은 가격순', value: 'price_asc' },
+    { label: '높은 가격순', value: 'price_desc' },
+  ];
+
 
   const [sortBy, setsortBy] = useState<sortByOption>('recent')
   const [modalState, setModalState] = useState<ModalState | null>(null)
   const { data, isPending, isError } = useRequestList(sortBy)
+
 
   if (isPending) return <div>로딩 중...</div>
   if (isError) return <div>에러남</div>
@@ -25,7 +32,7 @@ export default function PurchaseRequestManagePage() {
           <h1 className="text-[18px] font-bold tracking-[-0.45px] text-black max-sm:text-[16px] max-sm:tracking-[-0.4px]">
             구매 요청 관리
           </h1>
-          <SortDropdown />
+          <SortDropdown options={SORT_OPTIONS} value={sortBy} onChange={(value) => setsortBy(value as sortByOption)} />
         </div>
 
         <div className="flex w-full flex-col items-end gap-[30px] max-sm:gap-5">
@@ -50,6 +57,7 @@ export default function PurchaseRequestManagePage() {
             </div>
 
 
+
             <ul className="flex w-full min-w-[1100px] flex-col max-lg:min-w-[696px]">
               {data.map((request) => (
                 <li
@@ -63,7 +71,7 @@ export default function PurchaseRequestManagePage() {
                     {request.itemSummary}
                   </span>
                   <span className="w-[142px] shrink-0 text-[16px] tracking-[-0.4px] text-gray-950 max-lg:w-[100px]">
-                    {request.totalAmount}
+                    {request.totalAmount.toLocaleString()}
                   </span>
                   <div className="flex w-[134px] shrink-0 items-center gap-3 max-lg:w-[108px]">
                     <span className="w-[90px] text-[16px] tracking-[-0.4px] text-gray-950 max-lg:w-16">
@@ -106,7 +114,7 @@ export default function PurchaseRequestManagePage() {
                       {request.itemSummary}
                     </p>
                     <p className="text-[20px] font-extrabold tracking-[-0.5px] text-gray-950">
-                      {request.totalAmount}원
+                      {request.totalAmount.toLocaleString()}
                     </p>
                   </div>
                 </div>
